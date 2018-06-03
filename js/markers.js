6d39e132eab09eed0330e63fcebb24dd4c65092e
insomnolent <jbcccsun@gmail.com>
2018-06-03 16:56:39 -0700

    add shiny app

--- js/markers.js
+++ js/markers.js
@@ -8,35 +8,82 @@ var marquise = document.getElementById('marquise-text');
 
 var whole_map = document.getElementById('Map');
 
+// all sections for auditorium
+var allSection = ["#stage-text-show", "#audience-text-show", "#fountain-text-show", "#stairs-text-show", "#foyer-text-show", "#tickets-text-show", "#marquise-text-show"];
 
+// initially hide all descriptions
+for (i = 0; i < allSection.length; i++) {
+	var section = allSection[i];
+	$(`${section}`).attr('style', 'display: none');
+}
+
+// hide all other descriptions except for one that was just clicked
+function hideAllExcept(showID) {
+	for (i = 0; i < allSection.length; i++) {
+		if (allSection[i] != showID) {
+			var section = allSection[i];
+			$(`${section}`).attr('style', 'display: none');
+		}
+	}
+}
 
 stage.onclick = function() {
-	$("#text-show").text('this is the stage');
-
-	var pic1 = document.createElement("img");
-	var pic2 = document.createElement("img");
-	pic1.src = 'img/floor_plan_boxes.png';
-	pic2.src = 'img/floor_plan.png';
-	pic1.setAttribute("style", "border: 2px solid blue; width: 200px;");
-	pic2.setAttribute("style", "border: 2px solid blue; width: 200px;");
-	$("#text-show").append(pic1);
-	$("#text-show").append(pic2);
+	$('#stage-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[0]);
 }
 audience.onclick = function() {
-	$("#text-show").text('audience sits here');
+	$('#audience-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[1]);
 }
 fountain.onclick = function() {
-	$("#text-show").text('this is a crystal fountain');
+	$('#fountain-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[2]);
 }
 stairs.onclick = function() {
-	$("#text-show").text('go up the stairs');
+	$('#stairs-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[3]);
 }
 foyer.onclick = function() {
-	$("#text-show").text('FOYER');
+	$('#foyer-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[4]);
+
 }
 tickets.onclick = function() {
-	$("#text-show").text('a ticketing booth');
+	$('#tickets-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[5]);
 }
 marquise.onclick = function() {
-	$("#text-show").text("what's a marquise");
+	$('#marquise-text-show').attr('style', 'display: block');
+	hideAllExcept(allSection[6]);
 }
+
+// stage.onclick = function() {
+// 	$("#text-show").text('this is the stage');
+
+// 	var pic1 = document.createElement("img");
+// 	var pic2 = document.createElement("img");
+// 	pic1.src = 'img/floor_plan_boxes.png';
+// 	pic2.src = 'img/floor_plan.png';
+// 	pic1.setAttribute("style", "border: 2px solid blue; width: 200px;");
+// 	pic2.setAttribute("style", "border: 2px solid blue; width: 200px;");
+// 	$("#text-show").append(pic1);
+// 	$("#text-show").append(pic2);
+// }
+// audience.onclick = function() {
+// 	$("#text-show").text('audience sits here');
+// }
+// fountain.onclick = function() {
+// 	$("#text-show").text('this is a crystal fountain');
+// }
+// stairs.onclick = function() {
+// 	$("#text-show").text('go up the stairs');
+// }
+// foyer.onclick = function() {
+// 	$("#text-show").text('FOYER');
+// }
+// tickets.onclick = function() {
+// 	$("#text-show").text('a ticketing booth');
+// }
+// marquise.onclick = function() {
+// 	$("#text-show").text("what's a marquise");
+// }
